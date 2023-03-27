3e71415c5f6dc47ec024e907b07a87d721a583ca
Melak <km250542@ncr.com>
2023-03-27 10:17:31 +0200

    Set the main

--- main.c
+++ main.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "triangle.c"
 #include "circle.c"
 
 int main()
 {
-    float radius;
+    int chosenCase;
+
+    bool flag = true;
+    bool *flagPointer;
+    flagPointer = &flag;
+
+
+    printf("Hello and welcome to my first C project.\nThis application has some math functions.\n");
+    printf("1. To calculate circumference and area of a circle with provided radius.\n");
+    printf("2. To calculate area of a right triangle with user specified side lengths.\n");
+
+   
+   while (flag)
+   {
+    input:
+    while (getchar() != '\n');
+    printf("PRESS THE NUMBER\n");
+    int validFormat = scanf("%d", &chosenCase);
+
+    if(validFormat == 0)
+    {
+        printf("Invalid input!\n");
+        goto input;
+    }
     
-    printf("Enter the radius: \n");
-    scanf("%f", &radius);
+    if(chosenCase <= 3 && chosenCase > 0)
+         *flagPointer = false;
+   }
 
-    haha();
-
-    double test = circumference(&radius);
-    double test2 = area(&radius);
-
-    printf("Circumference: %lf\n", test);
-    printf("Area: %lf", test2);
+   printf("You chosed: %d", chosenCase);
    
     return 0;
 }
