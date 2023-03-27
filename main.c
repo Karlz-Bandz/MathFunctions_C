be838cfddea6fa28ac66467497499b83bcad4cf3
Melak <km250542@ncr.com>
2023-03-27 11:03:47 +0200

    Input catch exceptions works

--- main.c
+++ main.c
@@ -1,40 +1,52 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
 #include "triangle.c"
 #include "circle.c"
 
 int main()
 {
-    int chosenCase;
+    float chosenTemp;
+    float *chosenTempPtr;
+    chosenTempPtr = &chosenTemp;
 
     bool flag = true;
     bool *flagPointer;
     flagPointer = &flag;
 
+    int validFormat = 1;
+    int *validFormatPtr;
+    validFormatPtr = &validFormat;
 
     printf("Hello and welcome to my first C project.\nThis application has some math functions.\n");
     printf("1. To calculate circumference and area of a circle with provided radius.\n");
     printf("2. To calculate area of a right triangle with user specified side lengths.\n");
 
-   
-   while (flag)
-   {
-    input:
-    while (getchar() != '\n');
-    printf("PRESS THE NUMBER\n");
-    int validFormat = scanf("%d", &chosenCase);
-
-    if(validFormat == 0)
+while (flag)
+{
+    if(*validFormatPtr == 0)
     {
         printf("Invalid input!\n");
-        goto input;
     }
+    printf("PRESS THE NUMBER\n");
     
-    if(chosenCase <= 3 && chosenCase > 0)
-         *flagPointer = false;
-   }
+    *validFormatPtr = scanf("%f", chosenTempPtr);
 
-   printf("You chosed: %d", chosenCase);
+    while (getchar() != '\n');
+
+    if(*chosenTempPtr <= 3 && *chosenTempPtr > 0 &&
+     *chosenTempPtr == ceil(*chosenTempPtr) &&
+      *chosenTempPtr == floor(*chosenTempPtr))
+    {
+         *flagPointer = false;
+    }
+    else
+    {
+         *validFormatPtr = 0;
+    }
+}
+
+   printf("You chosed: %f", *chosenTempPtr);
    
     return 0;
 }
